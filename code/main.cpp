f0423b1474f08426160adbf40ed3a798018cf5c8
canq.chen <canq.chen@gmail.com>
2021-03-13 01:26:53 +0800

    change some configs

--- code/main.cpp
+++ code/main.cpp
@@ -1,18 +1,13 @@
-/*
- * @Author       : mark
- * @Date         : 2020-06-18
- * @copyleft Apache 2.0
- */ 
 #include <unistd.h>
 #include "server/WebServer.h"
 
 int main() {
-    /* 守护进程 后台运行 */
+    // 守护进程 后台运行
     //daemon(1, 0); 
 
     WebServer server(
-        1316, 3, 60000, false,             /* 端口 ET模式 timeoutMs 优雅退出  */
-        3306, "root", "root", "webserver", /* MySQL配置 端口 用户名 密码 数据库名称*/
+        1314, 3, 60000, false,             /* 端口 ET模式 timeoutMs 优雅退出  */
+        3306, "root", "passwd", "userInfo", /* MySQL配置 端口 用户名 密码 数据库名称*/
         12, 6, true, 1, 1024);             /* MySQL连接池数量 线程池数量 日志开关 日志等级 日志异步队列容量 */
     server.Start();
 } 
