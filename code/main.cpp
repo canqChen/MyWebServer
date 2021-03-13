42768fb97378cf9f2ee4015f8618c7850823ce15
canq.chen <canq.chen@gmail.com>
2021-03-13 14:28:21 +0800

    modified pool num

--- code/main.cpp
+++ code/main.cpp
@@ -8,7 +8,7 @@ int main() {
     WebServer server(
         1314, 3, 60000, false,             /* 端口 ET模式 timeoutMs 优雅退出  */
         3306, "root", "passwd", "userInfo", /* MySQL配置 端口 用户名 密码 数据库名称*/
-        12, 6, true, 1, 1024);             /* MySQL连接池数量 线程池数量 日志开关 日志等级 日志异步队列容量 */
+        10, 12, true, 1, 1024);             /* MySQL连接池数量 线程池数量 日志开关 日志等级 日志异步队列容量 */
     server.Start();
 } 
   
